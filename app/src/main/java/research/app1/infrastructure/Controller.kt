35a4b95cb009aa2c4178a303fff47764d1d29713
michael dudley <michael.dudley@improvingenterprises.com>
2018-03-09 14:17:00 -0600

    adding a fade animation

--- app/src/main/java/research/app1/infrastructure/Controller.kt
+++ app/src/main/java/research/app1/infrastructure/Controller.kt
@@ -1,8 +1,14 @@
 package research.app1.infrastructure
 
+import android.animation.ObjectAnimator
 import android.app.Activity
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
+import android.transition.Fade
+import android.transition.Fade.IN
+import android.transition.Fade.OUT
+import android.transition.Scene
+import android.transition.TransitionManager
 import android.view.View
 
 open class Controller {
@@ -18,6 +24,7 @@ open class Controller {
     }
 
     fun push(view: View) : View {
+        animate(view)
         views.add(view)
         region.addView(view)
         return view
@@ -25,6 +32,44 @@ open class Controller {
 
     fun <T : ViewDataBinding?>push(view: Int) : T {
         return DataBindingUtil.bind<T>(push(view))
+    }
+
+    fun animate(view: View) : View {
+
+        //With property animator
+        val shortDuration = 5000L
+
+        //out with the old view if it exists
+        if(views.any()){
+            views.last().animate().apply {
+                alpha(0f)
+                duration = shortDuration
+                //setListener(null)
+            }
+        }
+        //in with the new view
+        view.alpha = 0f
+        view.visibility = View.VISIBLE
+        view.animate().apply {
+            alpha(1f)
+            duration = shortDuration
+            //setListener(null)
+        }
+
+        //With Scene and Transition Manager
+//        if(views.any()){
+//            val scene = Scene(region, views.last())
+//            TransitionManager.go(scene, Fade(OUT))
+//        }
+
+        //With ObjectAnimator
+//        view.x = -500f
+//        ObjectAnimator.ofFloat(view, "translationX", 0f).apply {
+//            duration = 500
+//            start()
+//        }
+
+        return view
     }
 
     companion object {
