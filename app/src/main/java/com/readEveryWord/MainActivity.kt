aea7bb1a936d54bac76445f0edc622269f93eac7
Craig Neuwirt <cneuwirt@gmail.com>
2018-10-18 15:24:28 -0500

    Bootstrap activity

--- app/src/main/java/com/readEveryWord/MainActivity.kt
+++ app/src/main/java/com/readEveryWord/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.readEveryWord
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.miruken.callback.TypeHandlers
 import com.miruken.context.Context
 import com.miruken.mvc.Navigator
 import com.miruken.mvc.android.ViewRegion
+import com.miruken.mvc.next
 import com.readEveryWord.features.books.BooksController
 
 class MainActivity : AppCompatActivity() {
-    val _appContext = Context()
+    private val _appContext = Context()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +19,12 @@ class MainActivity : AppCompatActivity() {
         val navigator  = Navigator(mainRegion)
         _appContext.addHandlers(navigator, TypeHandlers)
 
-        Controller.activity = this
-        Controller.region   = Region(this)
-        setContentView(Controller.region)
-        BooksController().showBooks()
+        setContentView(mainRegion)
+        _appContext.next<BooksController> { showBooks() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _appContext.end()
     }
 }
