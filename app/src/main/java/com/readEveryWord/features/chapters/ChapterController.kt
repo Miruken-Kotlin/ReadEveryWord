916e833a0c872e39f892a491c4323993c94257a7
michael dudley <michael.dudley@improvingenterprises.com>
2018-10-23 12:54:09 -0500

    updating to use injection

--- app/src/main/java/com/readEveryWord/features/chapters/ChapterController.kt
+++ app/src/main/java/com/readEveryWord/features/chapters/ChapterController.kt
@@ -3,7 +3,9 @@ package com.readEveryWord.features.chapters
 import android.content.Context
 import android.view.ViewGroup
 import com.android.databinding.library.baseAdapters.BR
+import com.miruken.callback.Provides
 import com.miruken.callback.resolve
+import com.miruken.context.Scoped
 import com.miruken.mvc.android.AndroidController
 import com.readEveryWord.R
 import com.readEveryWord.data.ReadingRecordData
@@ -13,13 +15,21 @@ import com.readEveryWord.domain.Book
 import com.readEveryWord.domain.Chapter
 import java.util.*
 
-class ChapterController(
-        val book:    Book,
-        val chapter: Chapter
-) : AndroidController() {
-    fun showChapter(parent: ViewGroup) {
+class ChapterController
+    @Provides @Scoped
+    constructor()  : AndroidController() {
+
+    private lateinit var book:    Book
+
+    lateinit var chapter: Chapter
+
+    fun showChapter(book: Book, chapter: Chapter) {
+        this.book    = book
+        this.chapter = chapter
+
         show(R.layout.chapters_chapter, BR.ctrl)
     }
+
     fun markAsRead(){
         createReadingRecord(ReadingRecordData(
                 book.id,
--- app/src/main/java/com/readEveryWord/features/chapters/ChaptersController.kt
+++ app/src/main/java/com/readEveryWord/features/chapters/ChaptersController.kt
@@ -3,6 +3,8 @@ package com.readEveryWord.features.chapters
 import android.widget.TableLayout
 import android.widget.TableRow
 import com.android.databinding.library.baseAdapters.BR
+import com.miruken.callback.Provides
+import com.miruken.context.Scoped
 import com.miruken.mvc.android.AndroidController
 import com.miruken.mvc.android.component.table
 import com.miruken.mvc.push
@@ -10,17 +12,19 @@ import com.readEveryWord.R
 import com.readEveryWord.domain.Book
 import com.readEveryWord.domain.Chapter
 
-class ChaptersController : AndroidController()
+class ChaptersController
+    @Provides @Scoped
+    constructor() : AndroidController()
 {
     lateinit var book: Book
 
-    fun showChapters(selectedBook: Book){
-        book = selectedBook
+    fun showChapters(book: Book) {
+        this.book = book
 
         show(R.layout.chapters_chapter, BR.ctrl){
             table(this, R.id.chapter_table, 6).apply {
                  book.chapters.forEach {
-                    add().push<ChaptersController> { showChapters(this.book) }
+                    add().push<ChapterController> { showChapter(book, it) }
                  }
             }
         }
