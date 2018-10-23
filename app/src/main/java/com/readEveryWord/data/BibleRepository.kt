2ea79ce868cde735fe23c9edaafebfb3759ed5c2
Craig Neuwirt <cneuwirt@gmail.com>
2018-10-23 14:37:55 -0500

    Moving queries into BibleReposutory

--- app/src/main/java/com/readEveryWord/data/BibleRepository.kt
+++ app/src/main/java/com/readEveryWord/data/BibleRepository.kt
@@ -4,14 +4,30 @@ import android.content.Context
 import com.miruken.callback.Handler
 import com.miruken.callback.Provides
 import com.miruken.callback.Singleton
+import com.readEveryWord.data.queries.createReadingRecord
 import com.readEveryWord.data.queries.getAllReadingRecords
 import com.readEveryWord.domain.Bible
+import com.readEveryWord.domain.Book
+import com.readEveryWord.domain.Chapter
+import java.util.*
 
-object BibleRepository : Handler() {
+class BibleRepository
+    @Provides @Singleton constructor(
+            val context: Context
+    ) : Handler() {
+
     @Provides @Singleton
-    fun getBible(context: Context) = Bible().apply {
+    fun getBible() = Bible().apply {
         getAllReadingRecords(context).forEach {
             books[it.bookId].chapters[it.chapterId].read = true
         }
+    }
+
+    fun markChapterAsRead(chapter: Chapter, book: Book) {
+        createReadingRecord(ReadingRecordData(
+                book.id, chapter.id,
+                Date().time, 0,
+                null, null), context)
+        chapter.read = true
     }
 }
--- app/src/main/java/com/readEveryWord/features/chapters/ChapterController.kt
+++ app/src/main/java/com/readEveryWord/features/chapters/ChapterController.kt
@@ -2,19 +2,17 @@ package com.readEveryWord.features.chapters
 
 import com.android.databinding.library.baseAdapters.BR
 import com.miruken.callback.Provides
-import com.miruken.callback.resolve
 import com.miruken.context.Scoped
 import com.miruken.mvc.android.AndroidController
 import com.readEveryWord.R
-import com.readEveryWord.data.ReadingRecordData
-import com.readEveryWord.data.queries.createReadingRecord
+import com.readEveryWord.data.BibleRepository
 import com.readEveryWord.domain.Book
 import com.readEveryWord.domain.Chapter
-import java.util.*
 
 class ChapterController
-    @Provides @Scoped
-    constructor() : AndroidController() {
+    @Provides @Scoped constructor(
+            private val repository: BibleRepository
+    ) : AndroidController() {
 
     private lateinit var book: Book
 
@@ -28,12 +26,6 @@ class ChapterController
     }
 
     fun markAsRead() {
-        createReadingRecord(ReadingRecordData(
-                book.id,
-                chapter.id,
-                Date().time,
-                0, null,null),
-                context!!.resolve()!!)
-        chapter.read = true
+        repository.markChapterAsRead(chapter, book)
     }
 }
